d9f526fe413b4948422d2a8cbdaca1c25bc5ef37
Yaroslav @nilfalse <nilfalse@yandex.com>
2019-06-30 23:04:01 +0200

    build React Flickr example

---
+++ 2019_flickr-react/build/precache-manifest.47ff2f8008a07a0db2e1e1aaf7563590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67efd700fd656bef1e1e328766a3b53c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "701f8618a7aa135daebd",
+    "url": "./static/css/main.90fc9e13.chunk.css"
+  },
+  {
+    "revision": "1a89a79411283c13d9a2",
+    "url": "./static/js/2.f005262b.chunk.js"
+  },
+  {
+    "revision": "701f8618a7aa135daebd",
+    "url": "./static/js/main.143c5583.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
